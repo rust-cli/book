334c8e9e9c068fe56afee426f11348b6a817ff43
Yi Hyunjoon <yi.hyunjoon@gmail.com>
2020-09-15 18:32:06 +0900

    Replace`failure` with `anyhow` in signals-channels

--- src/in-depth/signals-channels.rs
+++ src/in-depth/signals-channels.rs
@@ -1,5 +1,6 @@
 use std::time::Duration;
 use crossbeam_channel::{bounded, tick, Receiver, select};
+use anyhow::Result;
 
 fn ctrl_channel() -> Result<Receiver<()>, ctrlc::Error> {
     let (sender, receiver) = bounded(100);
@@ -10,7 +11,7 @@ fn ctrl_channel() -> Result<Receiver<()>, ctrlc::Error> {
     Ok(receiver)
 }
 
-fn main() -> Result<(), exitfailure::ExitFailure> {
+fn main() -> Result<()> {
     let ctrl_c_events = ctrl_channel()?;
     let ticks = tick(Duration::from_secs(1));
 
