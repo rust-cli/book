31e5129b4145c5baf04065fcd134bbce6c429cc0
epage <epage@users.noreply.github.com>
2021-12-07 02:42:23 +0000

    deploy: bb4c87921307a9d84fecdd70fcf9cdc94e2af9f1

--- tutorial/testing/tests/cli.rs
+++ tutorial/testing/tests/cli.rs
@@ -9,18 +9,17 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
     cmd.arg("foobar").arg("test/file/doesnt/exist");
     cmd.assert()
         .failure()
-        .stderr(predicate::str::contains("No such file or directory"));
+        .stderr(predicate::str::contains("could not read file"));
 
     Ok(())
 }
 
-use std::io::{self, Write};
-use tempfile::NamedTempFile;
+use assert_fs::prelude::*;
 
 #[test]
 fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
-    let mut file = NamedTempFile::new()?;
-    writeln!(file, "A test\nActual content\nMore content\nAnother test")?;
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("A test\nActual content\nMore content\nAnother test")?;
 
     let mut cmd = Command::cargo_bin("grrs")?;
     cmd.arg("test").arg(file.path());
