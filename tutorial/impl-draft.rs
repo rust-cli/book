21da9f915c8580806f3a92aaa45560e7fb872951
epage <epage@users.noreply.github.com>
2022-01-03 12:27:29 +0000

    deploy: e8ca51cc679cbabaa47a97f8f4698ea00cc28ec0

--- tutorial/impl-draft.rs
+++ tutorial/impl-draft.rs
@@ -14,14 +14,14 @@ struct Cli {
 
 fn main() {
 
-let args = Cli::from_args();
-let content = std::fs::read_to_string(&args.path)
-    .expect("could not read file");
+    let args = Cli::from_args();
+    let content = std::fs::read_to_string(&args.path)
+        .expect("could not read file");
 
-for line in content.lines() {
-    if line.contains(&args.pattern) {
-        println!("{}", line);
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
     }
-}
 
 }
