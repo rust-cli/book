b48308b032345635e646c624670b245cb152eb7c
Brett Chalupa <brettchalupa@gmail.com>
2022-10-26 15:55:34 -0400

    Iterate on machine-communication-stdin
    Based on feedback

--- src/in-depth/machine-communication-stdin.rs
+++ src/in-depth/machine-communication-stdin.rs
@@ -19,16 +19,16 @@ fn main() {
     if file == PathBuf::from("-") {
         if atty::is(Stream::Stdin) {
             Cli::command().print_help().unwrap();
-            ::std::process::exit(0);
+            ::std::process::exit(2);
         }
 
-        file = PathBuf::from("stdin");
+        file = PathBuf::from("<stdin>");
         word_count = words_in_buf_reader(BufReader::new(stdin().lock()));
     } else {
         word_count = words_in_buf_reader(BufReader::new(File::open(&file).unwrap()));
     }
 
-    println!("Words from {}: {}", file.to_str().unwrap(), word_count)
+    println!("Words from {}: {}", file.to_string_lossy(), word_count)
 }
 
 fn words_in_buf_reader<R: BufRead>(buf_reader: R) -> usize {
